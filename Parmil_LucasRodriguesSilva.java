bda17c745b04d8b40345138fd7354e2a3e01914d
Lucas-Rodrigues-Silva <73086875+Lucas-Rodrigues-Silva@users.noreply.github.com>
2020-11-23 18:09:10 -0300

    Add files via upload

---
+++ Parmil_LucasRodriguesSilva.java
@@ -0,0 +1,22 @@
+package parmil_lucasrodriguessilva;
+/**
+ *
+ * @author Menino Moleque Luquinha
+ */
+public class Parmil_LucasRodriguesSilva {
+    
+    public static void main(String[] args) {
+        
+        int i=1000;
+        
+        do{
+            if( i % 2 == 0 ){
+                
+                System.out.println(i);
+                i++;
+            }
+            else{ i++; }
+        }while(i <= 1500);
+    }
+    
+}
